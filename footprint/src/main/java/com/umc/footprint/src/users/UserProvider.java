a4243f5405c30cca36ac39505241783ec68a09a8
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-01-16 11:37:24 +0900

    new : 유저 정보 조회 API

--- footprint/src/main/java/com/umc/footprint/src/users/UserProvider.java
+++ footprint/src/main/java/com/umc/footprint/src/users/UserProvider.java
@@ -3,6 +3,14 @@ package com.umc.footprint.src.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.umc.footprint.config.BaseException;
+import com.umc.footprint.src.users.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import static com.umc.footprint.config.BaseResponseStatus.*;
+
 @Service
 public class UserProvider {
 
@@ -11,5 +19,15 @@ public class UserProvider {
     @Autowired
     public UserProvider(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    // 해당 userIdx를 갖는 User의 정보 조회
+    public GetUserRes getUser(int userIdx) throws BaseException {
+        try {
+            GetUserRes getUserRes = userDao.getUser(userIdx);
+            return getUserRes;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 }
