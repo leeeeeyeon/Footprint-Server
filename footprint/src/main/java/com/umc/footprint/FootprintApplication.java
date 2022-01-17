37610581149282e3ca42169281924442e5ebab67
dlxortmd <dlxortmd987@gamil.com>
2022-01-17 13:47:03 +0900

    new : aws S3 관련 setting application.yml, aws.yml 파일 resources에추가 필요함 내용은 slack 참조

--- footprint/src/main/java/com/umc/footprint/FootprintApplication.java
+++ footprint/src/main/java/com/umc/footprint/FootprintApplication.java
@@ -1,13 +1,20 @@
 package com.umc.footprint;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class FootprintApplication {
 
+	// 설정 파일 사용 (application.yml, aws.yml)
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(FootprintApplication.class, args);
+		new SpringApplicationBuilder(FootprintApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
