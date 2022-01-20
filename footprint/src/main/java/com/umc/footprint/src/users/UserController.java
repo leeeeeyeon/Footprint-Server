e67e8def4bf2de66f70cde562f898206fdbf29d4
minaver <psmjoshua@gmail.com>
2022-01-20 22:55:45 +0900

    improve : [추가] Validation 1~6

--- footprint/src/main/java/com/umc/footprint/src/users/UserController.java
+++ footprint/src/main/java/com/umc/footprint/src/users/UserController.java
@@ -3,10 +3,13 @@ package com.umc.footprint.src.users;
 
 
 
+import com.umc.footprint.config.BaseResponseStatus;
 import com.umc.footprint.src.users.model.GetUserTodayRes;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.umc.footprint.src.users.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,6 +72,40 @@ public class UserController {
     @ResponseBody
     @PatchMapping("/{useridx}/goals") // [PATCH] /users/:useridx/goals
     public BaseResponse<String> modifyGoal(@PathVariable("useridx") int userIdx, @RequestBody PatchUserGoalReq patchUserGoalReq){
+
+
+        // Validaion 1. userIdx 가 0 이하일 경우 exception
+        if(userIdx <= 0)
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.INVALID_USERIDX).getStatus());
+
+        // Validaion 2. dayIdx 길이 확인
+        if(patchUserGoalReq.getDayIdx().size() == 0) // 요일 0개 선택
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.MIN_DAYIDX).getStatus());
+        if(patchUserGoalReq.getDayIdx().size() > 7)  // 요일 7개 초과 선택
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.MAX_DAYIDX).getStatus());
+
+        // Validaion 3. dayIdx 숫자 범위 확인
+        for (Integer dayIdx : patchUserGoalReq.getDayIdx()){
+            if (dayIdx > 7 || dayIdx < 1)
+                return new BaseResponse<>(new BaseException(BaseResponseStatus.INVALID_DAYIDX).getStatus());
+        }
+
+        // Validaion 4. dayIdx 중복된 숫자 확인
+        Set<Integer> setDayIDx = new HashSet<>(patchUserGoalReq.getDayIdx());
+        if(patchUserGoalReq.getDayIdx().size() != setDayIDx.size()) // dayIdx 크기를 set으로 변형시킨 dayIdx 크기와 비교. 크기가 다르면 중복된 값 존재
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.OVERLAP_DAYIDX).getStatus());
+
+        // Validaion 5. walkGoalTime 범위 확인
+        if(patchUserGoalReq.getWalkGoalTime() < 10) // 최소 산책 목표 시간 미만
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.MIN_WALK_GOAL_TIME).getStatus());
+        if(patchUserGoalReq.getWalkGoalTime() > 720) // 최대 산책 목표 시간 초과
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.MAX_WALK_GOAL_TIME).getStatus());
+
+        // Validaion 6. walkTimeSlot 범위 확인
+        if(patchUserGoalReq.getWalkTimeSlot() > 8 || patchUserGoalReq.getWalkTimeSlot() < 1)
+            return new BaseResponse<>(new BaseException(BaseResponseStatus.INVALID_WALK_TIME_SLOT).getStatus());
+
+
         try {
             userService.modifyGoal(userIdx, patchUserGoalReq);
 
