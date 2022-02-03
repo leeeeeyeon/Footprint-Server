6d2d179397c92e21c311e17589fa7ca153cca826
Sangmin Park <83809461+minaver@users.noreply.github.com>
2022-02-04 02:08:19 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,16 @@ APP_NAME=footprint #1
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+# CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
-fi
+# if [ -z $CURRENT_PID ] #2
+# then
+#   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+# else
+#   echo "> kill -15 $CURRENT_PID"
+#   sudo kill -15 $CURRENT_PID
+#   sleep 5
+# fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /var/www/dev/deploy/footprint/build/libs/footprint-0.0.1-SNAPSHOT.jar
+nohup java -jar $JAR_PATH #/var/www/dev/deploy/footprint/build/libs/footprint-0.0.1-SNAPSHOT.jar
