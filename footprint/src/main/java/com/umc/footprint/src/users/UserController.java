0dbf1d64ca2e2a9340fec6de6fcf5ddd4afbb611
minaver <psmjoshua@gmail.com>
2022-02-06 23:20:46 +0900

    fix:[고침] List 형식의 response -> 하나의 Object

--- footprint/src/main/java/com/umc/footprint/src/users/UserController.java
+++ footprint/src/main/java/com/umc/footprint/src/users/UserController.java
@@ -66,15 +66,16 @@ public class UserController {
     // Path-variable
     @ResponseBody
     @GetMapping("/today")
-    public BaseResponse<List<GetUserTodayRes>> getToday(){
+    public BaseResponse<GetUserTodayRes> getToday(){
         try{
-            // userId(구글이나 카카오에서 보낸 ID) 추출 (복호화)
-            String userId = jwtService.getUserId();
-            System.out.println("userId = " + userId);
-            // userId로 userIdx 추출
-            int userIdx = userProvider.getUserIdx(userId);
+//            // userId(구글이나 카카오에서 보낸 ID) 추출 (복호화)
+//            String userId = jwtService.getUserId();
+//            System.out.println("userId = " + userId);
+//            // userId로 userIdx 추출
+//            int userIdx = userProvider.getUserIdx(userId);
+            int userIdx = 44;
 
-            List<GetUserTodayRes> userTodayRes = userProvider.getUserToday(userIdx);
+            GetUserTodayRes userTodayRes = userProvider.getUserToday(userIdx);
 
             return new BaseResponse<>(userTodayRes);
         } catch (BaseException exception){
