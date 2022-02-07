524f60928e7d06333fc4665ff85f4795d57c0e16
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-02-08 05:59:55 +0900

    fix : 사진 삭제 버그 해결

--- footprint/src/main/java/com/umc/footprint/src/footprints/FootprintService.java
+++ footprint/src/main/java/com/umc/footprint/src/footprints/FootprintService.java
@@ -55,14 +55,17 @@ public class FootprintService {
             // 전달되어온 파일 리스트
             List<MultipartFile> photos = patchFootprintReq.getPhotos();
 
+            System.out.println(photos);
+            System.out.println(photos.get(0).getOriginalFilename());
+            System.out.println(photos.isEmpty());
             if(photos != null) { // 본문만 수정(사진 수정 X)하는 경우 photos 자체가 null이 됨
                 if(dbPhotoList.isEmpty()) { // 발자국에 저장된 사진이 존재하지 않음
-                    if(!photos.isEmpty()) { // 전달된 파일이 하나라도 존재
+                    if(!("".equals(photos.get(0).getOriginalFilename()))) { // 전달된 파일이 하나라도 존재
                         uploadImg(photos, userIdx, footprintIdx); // 새로운 사진들 업로드
                     }
                 }
                 else { // 발자국에 저장된 기존 사진들이 존재
-                    if(photos.isEmpty()) { // 전달된 파일이 없음 > 사진을 지우고 싶다는 의미 > 기존 사진 삭제만 진행
+                    if(photos.size()==1 && "".equals(photos.get(0).getOriginalFilename())) { // 전달된 파일이 없음 > 사진을 지우고 싶다는 의미 > 기존 사진 삭제만 진행
                         footprintDao.deletePhotos(footprintIdx); // 기존 사진들 테이블에서 삭제
                     }
                     else {
