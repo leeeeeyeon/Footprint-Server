676b0226a085e9933d169b77fd056bb0268d9883
Sangmin Park <83809461+minaver@users.noreply.github.com>
2022-02-09 19:08:18 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,13 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-# if [ -z $CURRENT_PID ] 
-# then
-#     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-# else
-#   sudo kill -15 $CURRENT_PID
-#   sleep 5
-# fi
+if [ -z $CURRENT_PID ] 
+then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
 
 echo "> BUILD START" >> /var/www/dev/deploy/footprint/footprintServerLog.out
 
