c16fb89b221e0b666fb34ea5fbe3173b1ae63719
Sangmin Park <83809461+minaver@users.noreply.github.com>
2022-02-09 19:08:59 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ] 
 then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /var/www/dev/deploy/footprint/footprintServerLog.out
 else
   sudo kill -15 $CURRENT_PID
   sleep 5
