be1c24e8dd9fd6353d111129833b5e54191e4ba0
Sangmin Park <83809461+minaver@users.noreply.github.com>
2022-02-09 19:11:47 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,10 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
+echo "> BUILD START" >> /var/www/dev/deploy/footprint/footprintServerLog.out
+sudo ./gradlew clean build >> /var/www/dev/deploy/footprint/footprintServerLog.out 2>&1
+echo "> BUILD FINISH" >> /var/www/dev/deploy/footprint/footprintServerLog.out
+
 if [ -z $CURRENT_PID ] 
 then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /var/www/dev/deploy/footprint/footprintServerLog.out
@@ -16,12 +20,6 @@ else
   sudo kill -15 $CURRENT_PID
   sleep 5
 fi
-
-echo "> BUILD START" >> /var/www/dev/deploy/footprint/footprintServerLog.out
-
-sudo ./gradlew clean build >> /var/www/dev/deploy/footprint/footprintServerLog.out 2>&1
-
-echo "> BUILD FINISH" >> /var/www/dev/deploy/footprint/footprintServerLog.out
 
 echo "> $JAR_PATH 배포" >> /var/www/dev/deploy/footprint/footprintServerLog.out
 nohup java -jar $JAR_PATH > /var/www/dev/deploy/footprint/dev_footprint.log 2>&1 &
